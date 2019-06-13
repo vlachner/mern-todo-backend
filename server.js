d72a35fa2b672be0788ecc453f2b6e1f3eb66339
Vincenz <vini@wearehello.co>
2019-06-13 01:52:08 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const todoRoutes = express.Router()
+const PORT = 4000;
+
+let Todo = require('./todo.model');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = "mongodb+srv://dbUser:Pernix123.@pernixsandbox-saoxf.azure.mongodb.net/merntodo";
+mongoose.connect(uri, {useNewUrlParser: true});
+const connection = mongoose.connection;
+
+connection.once('open', function(){
+    console.log("MonogoDb Connected.")
+})
+
+
+app.use('/todos', todoRoutes);
+
+todoRoutes.route('/').get(function(req,res){
+    Todo.find(function(err,todos){
+        if(err){
+            console.log(err);
+        }else{
+            res.json(todos);
+        }
+    })
+})
+
+todoRoutes.route('/:id').get(function(req,res){
+    let id = req.params.id;
+    Todo.findById(id, function(err,todo){
+        if(err){
+            console.log(err);
+        }else{
+            res.json(todo);
+        }
+    })
+})
+
+todoRoutes.route('/add').post(function(req,res){
+    let todo = new Todo(req.body);
+    todo.save().then(todo => {
+        res.status(200).json({'todo':'todo added succesfully'})
+    }).catch(err =>{
+        res.status(400).send('Adding new todo Failed')
+    })
+})
+
+todoRoutes.route('/update/:id').post(function(req,res){
+    Todo.findById(req.params.id, function(err,todo){
+        if(!todo){
+            res.status(404).send('Data is not Found');
+        }else{
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+            todo.save().then(todo =>{
+                res.json('Todo Updated');
+            }).catch(err =>{
+                res.status(400).send('Update not possible');
+            })
+        }
+    })
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
